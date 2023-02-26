e57c18915ab7b2166432c0a663858bb403488f75
BJ <bing_john@163.com>
2023-02-26 09:20:15 +0800

    更新 适配flutter 3.7.5

--- flutter_weui_example/android/app/src/main/kotlin/com/example/flutter_weui_example/MainActivity.kt
+++ flutter_weui_example/android/app/src/main/kotlin/com/example/flutter_weui_example/MainActivity.kt
@@ -1,13 +1,22 @@
+//package com.example.flutter_weui_example
+//
+//import android.os.Bundle
+//
+//import io.flutter.app.FlutterActivity
+//import io.flutter.plugins.GeneratedPluginRegistrant
+//
+//class MainActivity: FlutterActivity() {
+//  override fun onCreate(savedInstanceState: Bundle?) {
+//    super.onCreate(savedInstanceState)
+//    GeneratedPluginRegistrant.registerWith(this)
+//  }
+//}
+
+// 2023-02-26
 package com.example.flutter_weui_example
 
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
 }
+
